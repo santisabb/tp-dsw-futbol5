6989328f77d7461a98216928693567ff51620ed2
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-09-09 02:24:57 -0300

    agrego directorios necesarios (hasta ahora) y lleno jsons con informacion

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+import { cors } from 'cors'
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:8081'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8081'
+    ]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, false)
+    }
+
+    return callback(new Error('Not allowed by CORS bro go fuck yourself'))
+  }
+})
